6428153abc53998ad24b39304e1e5505b7c81665
umairansari <umairansari.work@gmail.com>
2025-05-13 14:29:44 +0500

    feat: add sidebar navigation

---
+++ src/components/navigation/app-sidebar.tsx
@@ -0,0 +1,41 @@
+import {
+  Sidebar,
+  SidebarHeader,
+  SidebarFooter,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@/components/ui/sidebar';
+import { SIDEBAR_ITEMS } from '@/constants/sidebar-items';
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarHeader />
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {SIDEBAR_ITEMS.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  );
+}
---
+++ src/constants/sidebar-items.tsx
@@ -0,0 +1,50 @@
+import { Calendar, Home, Inbox, Search, Settings } from 'lucide-react';
+
+export const SIDEBAR_ITEMS = [
+  // {
+  //   group: 'Application',
+  //   icon: Home,
+  //   items: [
+  //     {
+  //       title: 'Dashboard',
+  //       url: '#',
+  //       icon: Home,
+  //     },
+  //     {
+  //       title: 'Reports',
+  //       url: '#',
+  //       icon: Inbox,
+  //     },
+  //     {
+  //       title: 'Analytics',
+  //       url: '#',
+  //       icon: Calendar,
+  //     },
+  //   ],
+  // },
+  {
+    title: 'Home',
+    url: '#',
+    icon: Home,
+  },
+  {
+    title: 'Inbox',
+    url: '#',
+    icon: Inbox,
+  },
+  {
+    title: 'Calendar',
+    url: '#',
+    icon: Calendar,
+  },
+  {
+    title: 'Search',
+    url: '#',
+    icon: Search,
+  },
+  {
+    title: 'Settings',
+    url: '#',
+    icon: Settings,
+  },
+];
